083d155ced3160190f28990d618402cedfa352b4
Mark Porter <mark.porter@gbatteries.com>
2025-01-27 07:55:55 -0500

    update w apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,65 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { useQuery, gql } from '@apollo/client';
+
 import './App.css'
+
+
+
+
+const GET_LOCATIONS = gql`
+
+  query GetLocations {
+
+    locations {
+
+      id
+
+      name
+
+      description
+
+      photo
+
+    }
+
+  }
+
+`;
+
+
+function DisplayLocations() {
+
+    const { loading, error, data } = useQuery(GET_LOCATIONS);
+  
+  
+    if (loading) return <p>Loading...</p>;
+  
+    if (error) return <p>Error : {error.message}</p>;
+  
+  
+    return data.locations.map(({ id, name, description, photo }) => (
+  
+      <div key={id}>
+  
+        <h3>{name}</h3>
+  
+        <img width="400" height="250" alt="location-reference" src={`${photo}`} />
+  
+        <br />
+  
+        <b>About this location:</b>
+  
+        <p>{description}</p>
+  
+        <br />
+  
+      </div>
+  
+    ));
+  
+  }
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +86,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <DisplayLocations/>
     </>
   )
 }
